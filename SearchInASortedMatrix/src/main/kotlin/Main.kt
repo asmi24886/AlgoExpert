de31e147c1f2e048342526802eb24681ea4f7bda
asmi2 <asmi24886@gmail.com>
2022-10-29 14:57:09 +0530

    All test cases passed

--- SearchInASortedMatrix/src/main/kotlin/Main.kt
+++ SearchInASortedMatrix/src/main/kotlin/Main.kt
@@ -5,3 +5,26 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
+fun searchInSortedMatrix(matrix: List<List<Int>>, target: Int): Pair<Int, Int> {
+    // Write your code here.
+    if(matrix.isEmpty())
+        return Pair(-1, -1)
+
+    var row = 0
+    var col = matrix[0].size -1
+
+    while(row < matrix.size && col >= 0) {
+
+        val value = matrix[row][col]
+        if(value == target) return Pair(row, col)
+        else if (value > target) {
+            col--
+        }
+        else {
+            row++
+        }
+    }
+
+    return Pair(-1, -1)
+}
